eeeb4257fff44e1f2868ae8da4335b32bbb0df56
Luna <advaith.madhukar@gmail.com>
2021-10-30 10:42:10 +0530

    fixed some logical errors, added comments uwu

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ mod clone;
 mod installdep;
 
 fn main() {
+    // check if ~/.dotfiles exists
     let dotfile_dir = shellexpand::tilde("~/.dotfiles").to_string();
     let path = Path::new(&dotfile_dir);
-    if !path.exists() {
+    if path.exists() {
         println!(
             "path {}, exists assuming dotfiles already cloned",
             dotfile_dir
         );
     } else {
+        // if it doesn't exist git clone it
         let _dotfile_repo = clone::clone_repo(path, "https://github.com/Lunarequest/Dotfiles");
     }
-    if installdep::exec_exists("stow") {
+    // check if stow is installed
+    if !installdep::exec_exists("stow") {
+        // if it isn't, install it
         match installdep::install_exec("stow") {
             Ok(()) => println!("installed stow"),
             Err(e) => panic!("{}", e),

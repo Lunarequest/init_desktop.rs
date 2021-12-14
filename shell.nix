27b973b906cf3cbcbcf5dc467d5a57ff9f2e7278
Luna D. Dragon <advaith.madhukar@gmail.com>
2021-12-14 12:28:46 +0530

    removed more debug add rust-ananlyzer to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-	nativeBuildInputs = with pkgs; [ openssl openssl.dev ];
+	nativeBuildInputs = with pkgs; [ openssl openssl.dev rust-analyzer ];
 	PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 }
